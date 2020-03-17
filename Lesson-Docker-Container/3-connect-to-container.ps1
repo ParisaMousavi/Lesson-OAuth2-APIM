7443e71aa2830842bd82769af17c8ebd40724fd9
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-03-17 10:54:26 +0100

    connect to the running container

---
+++ Lesson-Docker-Container/3-connect-to-container.ps1
@@ -0,0 +1,12 @@
+docker container run -it -p 5000:80 -e FLASK_APP=app.py --rm --name web2 -e FLASK_DEBUG=1 -v $PWD:/app web2
+
+# start the bash with container
+docker container exec -it web2 bash
+
+# execute a command on the running container to get the flask version
+docker container exec -it web2 flask --version
+
+# execute a command on the running container to create a file on the container
+docker container exec -it web2 touch hi.txt
+
+
