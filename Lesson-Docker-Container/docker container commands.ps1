0be43245323fd827d4230da580ba8a06980a7b8d
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-03-17 08:30:57 +0100

    working with docker container

--- Lesson-Docker-Container/docker container commands.ps1
+++ Lesson-Docker-Container/docker container commands.ps1
@@ -8,7 +8,23 @@ docker container  ls -a
 # -p means port. The second 5000 port is the port, which is assigned to flask.
 # -e means environment
 # --rm means remove the container automatically after it's stopped
-docker container run -it --rm -p 5000:5000 -e FLASK_APP=app.py web1
+# -e means environment
+docker container run -it --rm --name mycontainername -p 5000:80 -e FLASK_APP=app.py web1
 
-# remove container with <first four character of the container id>
-docker container rm 0000
+# run in dettached mode
+# --restart on-failure good for production
+docker container run -it --rm --name mycontainername -p 5000:80 -e FLASK_APP=app.py -d --restart on-failure web1
+
+# see the log of the container
+docker container logs mycontainername
+
+# see the log of the container in the real time
+docker container logs -f mycontainername
+    
+# matrix of real stats
+docker container stats
+
+# remove container 
+docker container rm contaier-name
+
+docker container stop contaier-name
