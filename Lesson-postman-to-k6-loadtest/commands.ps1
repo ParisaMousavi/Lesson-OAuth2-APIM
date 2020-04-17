3e56b4702e0e15fb4647131653a58e9ce4a9b89c
Parisa <p.moosavinezhad@tekaris.com>
2020-04-17 12:14:50 +0000

    small changes

--- Lesson-postman-to-k6-loadtest/commands.ps1
+++ Lesson-postman-to-k6-loadtest/commands.ps1
@@ -1,1 +1,6 @@
-testz
+
+# install npm on linux to get ready for postman-to-k6
+https://www.devroom.io/2011/10/24/installing-node-js-and-npm-on-ubuntu-debian/
+
+# load test with postman and K6
+https://k6.io/blog/load-testing-with-postman-collections
