b8886bc1e2276c3a48d31f815cb579ede428c680
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-04-20 14:00:00 +0200

    small chnages

--- Lesson-postman-to-k6-loadtest/commands.ps1
+++ Lesson-postman-to-k6-loadtest/commands.ps1
@@ -23,3 +23,7 @@ postman-to-k6 gPAS.postman_collection.json -i 1 -o k6-script.js
 
 k6 run k6-script.js
 
+k6 login cloud -t c5bb7c983c28f54efb6f4fc6d17b806ef4eb5290760ff9806b2286b8a33e9248
+
+k6 run k6-script.js
+k6 run -o cloud k6-script.js
